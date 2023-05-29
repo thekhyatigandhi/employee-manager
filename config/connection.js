8922e954f94ffb3c876c7ab30243e5debfc7ec61
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-29 17:23:22 -0400

    modified connections

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     host: "localhost",
     port: "3306",
     user: "root",
-    password: "Mysql@123",
+    password: "",
     database: "employee_db",
   },
   // confirm connection to db with yellow console log in terminal
