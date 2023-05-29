bab442f3c8e1b02185c5853a3be501a672a145c4
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-29 11:15:28 -0400

    modidfied index.js

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const inquirer = require("inquirer");
-const connection = require("./config/connection.js");
-require("console.table");
+const db = require("./config/connection.js");
+const selectTask = require("./lib/selectTask.js");
 
-const mainMenu = () => {
-  // http://www.figlet.org/fonts/big.flf
-  // http://www.figlet.org/fontdb_example.cgi?font=big.flf
-  console.log(`============Employee🖇Tracker=============`);
-  inquirer
-    .prompt({
-      name: "start",
-      type: "list",
-      message: "What would you like to do",
-      choices: [
-        "View All Employees", 
-        "View All Employees By Department", 
-        "View All Employees By Manager", 
-        "Add Employee", 
-        "Remove Employee", 
-        "Update Employee Role", 
-        "Update Employee Manager", 
-        "View All Roles", 
-        "Add Role", 
-        "Remove Role", 
-        "View All Departments", 
-        "Add Department", 
-        "Remove Department", 
-        "View Total Utilized Budget of a Department", 
-        "Exit", 
-      ],
-    })
+// handle errors or start application
+db.connect(function (err) {
+  if (err) throw err;
+  init();
+});
+
+// start application, show title special-text and present task list prompt (using 'selectTask')
+function init() {
+  displayTitleText();
+  selectTask();
+}
